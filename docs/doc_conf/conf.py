cc435b3657146d5d64025875894ee17075677237
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-12-09 23:37:02 +0000

    Added basic files

---
+++ docs/doc_conf/conf.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.sep.join((os.curdir, '..'))))
+
+project = 'BambuLabs API'
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon']
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build']
+autoclass_content = "both"
+
+html_theme = "sphinx_rtd_theme"
---
+++ tests/test_client.py
@@ -0,0 +1,8 @@
+import pytest
+import bambulabs_api as bl
+
+
+class TestServer:
+
+    def test_init(self):
+        assert True
