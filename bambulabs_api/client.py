80f2fcbf74a5c3857492e2325adaeca07286ebad
Chris Ioannidis <142539198+acse-ci223@users.noreply.github.com>
2023-12-10 03:41:01 +0000

    Updated docstrings and documentation

--- bambulabs_api/client.py
+++ bambulabs_api/client.py
@@ -1,14 +1,25 @@
+"""
+Client module for connecting to the Bambulabs 3D printer API
+and getting telemetry data from it in real time using MQTT protocol.
+"""
+
 from datetime import datetime, timedelta
 # from pathlib import Path
 
 import json
 import ssl
 
-import paho.mqtt.client as mqtt
-import webcolors
+import paho.mqtt.client as mqtt  # type: ignore
+import webcolors  # type: ignore
+
+
+__all__ = ['Client']
 
 
 class Client:
+    """
+    Client Class for connecting to the Bambulabs 3D printer
+    """
     def __init__(self, ip_address, access_code, serial, output_path="./"):
         self.ip_address = ip_address
         self.access_code = access_code
@@ -23,27 +34,99 @@ class Client:
         self.client.on_connect = self._on_connect
         self.client.on_message = self._on_message
 
-    def connect(self):
-        self.client.connect(self.ip_address, 8883, 60)
+    def connect(self) -> None:
+        """
+        connect Connect to the Bambulabs 3D printer
 
-    def _on_connect(self, client, userdata, flags, rc):
+        Returns
+        -------
+        None
+            Nothing
+        """
+        self.client.connect(self.ip_address, 8883, 60)
+        return None
+
+    def _on_connect(self, client, userdata, flags, rc) -> None:
+        """
+        _on_connect Callback function for when the client
+        receives a CONNACK response from the server.
+
+        Parameters
+        ----------
+        client : mqtt.Client
+            The client instance for this callback
+        userdata : String
+            User data
+        flags : Arraylike
+            Response flags sent by the broker
+        rc : int
+            The connection result
+        """
         print("Connected with result code " + str(rc))
         self.client.subscribe(f"device/{self.serial}/report")
+        return None
 
-    def _split_string(self, string):
-        tuple_result = (int(string[:2], 16), int(string[2:4], 16), int(string[4:6], 16))
+    def _split_string(self, string) -> tuple:
+        """
+        _split_string Split a string into a tuple of 3 integers
+
+        Parameters
+        ----------
+        string : String
+            String to split
+
+        Returns
+        -------
+        tuple
+            Tuple of 3 integers representing the RGB value
+        """
+        tuple_result = (int(string[:2], 16),
+                        int(string[2:4], 16),
+                        int(string[4:6], 16))
         return tuple_result
 
-    def _rgb_to_color_name(self, rgb):
+    def _rgb_to_color_name(self, rgb) -> str:
+        """
+        _rgb_to_color_name Convert an RGB value to a color name
+
+        Parameters
+        ----------
+        rgb : String
+            RGB value to convert to a color name (e.g. "FF0000")
+
+        Returns
+        -------
+        str
+            Color name (e.g. "red")
+        """
         try:
             color_tuple = self._split_string(rgb)
             color_name = webcolors.rgb_to_name(color_tuple)
         except ValueError:
-            # If the RGB value doesn't match any known color, return the hex code with a 0x prefeix
+            # If the RGB value doesn't match any known color,
+            # return the hex code with a 0x prefeix
             color_name = f"0x{rgb}"
         return color_name
 
-    def _on_message(self, client, userdata, msg):
+    def _on_message(self, client, userdata, msg) -> None:
+        """
+        _on_message Callback function for when a PUBLISH message
+        is received from the server.
+
+        Parameters
+        ----------
+        client : mqtt.Client
+            The client instance for this callback
+        userdata : String
+            User data
+        msg : mqtt.MQTTMessage
+            An instance of MQTTMessage. This is a class with members topic,
+            payload, qos, retain.
+
+        Returns
+        -------
+        None
+        """
         # Current date and time
         now = datetime.now()
         doc = json.loads(msg.payload)
@@ -60,7 +143,8 @@ class Client:
         # ams_text_path = output_dir / "ams.txt"
 
         try:
-            # Write the raw JSON first incase there's a key error when we start trying to access it
+            # Write the raw JSON first incase there's a key error
+            # when we start trying to access it
             # with telem_json_path.open("w") as fp:
             #     fp.write(json.dumps(doc))
 
@@ -80,12 +164,22 @@ class Client:
             future_time = now + timedelta(minutes=min_remain)
             future_time_str = future_time.strftime("%Y-%m-%d %H:%M")
 
-            active_ams = self.values['ams']['tray_now']
+            # active_ams = self.values['ams']['tray_now']
+
+            print(f"Layer: {layer} ({self.values['mc_percent']} %)\n"
+                  f"Nozzle Temp: {self.values['nozzle_temper']} / \
+                    {self.values['nozzle_target_temper']}\n"
+                  f"Bed Temp: {self.values['bed_temper']} / \
+                    {self.values['bed_target_temper']}\n"
+                  f"Finish ETA: {future_time_str}\n"
+                  f"Speed: {speed_map[speed]}")
 
             # with telem_text_path.open("w") as fp:
             #     fp.write(f"Layer: {layer} ({self.values['mc_percent']} %)\n"
-            #             f"Nozzle Temp: {self.values['nozzle_temper']}/{self.values['nozzle_target_temper']}\n"
-            #             f"Bed Temp: {self.values['bed_temper']}/{self.values['bed_target_temper']}\n"
+            #             f"Nozzle Temp: {self.values['nozzle_temper']}/
+            # {self.values['nozzle_target_temper']}\n"
+            #             f"Bed Temp: {self.values['bed_temper']}/
+            # {self.values['bed_target_temper']}\n"
             #             f"Finish ETA: {future_time_str}\n"
             #             f"Speed: {speed_map[speed]}")
 
@@ -103,16 +197,19 @@ class Client:
             #             tray_type = tray['tray_type']
             #         else:
             #             tray_type = tray['tray_sub_brands']
-            #         fp.write(f"Tray {tray['id']}: {tray_type} {color_name} {tray_remain}  {active}\n")
+            #         fp.write(f"Tray {tray['id']}:
+            # {tray_type} {color_name} {tray_remain}  {active}\n")
 
             #     if active_ams == "254":
             #         active = " - In Use"
             #     else:
             #         active = ""
 
-            #     color_name = self._rgb_to_color_name(self.values['vt_tray']['cols'][0])
+            #     color_name = self._rgb_to_color_name(
+            # self.values['vt_tray']['cols'][0])
 
-            #     fp.write(f"Ext. : {self.values['vt_tray']['tray_type']} {color_name} {active}")
+            #     fp.write(f"Ext. : {self.values['vt_tray']['tray_type']}
+            # {color_name} {active}")
 
         # Sometimes empty or diff doc structure returned
         # Swallow exception here and it'll just get retried next iteration
@@ -121,11 +218,35 @@ class Client:
             # with telem_json_err_path.open("w") as fp:
             #     fp.write(json.dumps(doc))
 
-    def publish(self, msg):
-        self.client.publish(f"device/{self.serial}/request", msg)
+        return None
 
-    def loop_forever(self):
+    def publish(self, msg) -> None:
+        """
+        publish Publish a message to the Bambulabs 3D printer
+
+        Parameters
+        ----------
+        msg : JSON
+            JSON message to send to the Bambulabs 3D printer
+
+        Returns
+        -------
+        None
+        """
+        self.client.publish(f"device/{self.serial}/request", json.dumps(msg))
+        return None
+
+    def loop_forever(self) -> None:
+        """
+        loop_forever Loop forever and process network traffic,
+        dispatches callbacks and handles reconnecting.
+
+        Returns
+        -------
+        None
+        """
         self.client.loop_forever()
+        return None
 
 
 # client = mqtt.Client()
@@ -135,7 +256,8 @@ class Client:
 # # Username isn't something you can change, so hardcoded here
 # client.username_pw_set('bblp', ACCESS_CODE)
 
-# # These 2 lines are required to bypass self signed certificate errors, at least on my machine
+# # These 2 lines are required to bypass self signed certificate errors,
+# at least on my machine
 # # these things can be finicky depending on your system setup
 # client.tls_set(tls_version=ssl.PROTOCOL_TLS, cert_reqs=ssl.CERT_NONE)
 # client.tls_insecure_set(True)
@@ -144,7 +266,8 @@ class Client:
 # client.on_message = on_message
 # client.connect(BAMBU_IP_ADDRESS, 8883, 60)
 
-# client.publish(f"device/{SERIAL}/request", '{"pushing": {"command": "start", "sequence_id": 0}}')
+# client.publish(f"device/{SERIAL}/request", '{"pushing":
+# {"command": "start", "sequence_id": 0}}')
 # # Blocking call that processes network traffic, dispatches callbacks and
 # # handles reconnecting.
 # # Other loop*() functions are available that give a threaded interface and a
--- examples/Basic/basic.py
+++ examples/Basic/basic.py
@@ -4,9 +4,18 @@ IP = '192.168.1.200'
 SERIAL = 'AC12309BH109'
 ACCESS_CODE = '12347890'
 
-# Create a new instance of the API
-api = bl.Client(IP, SERIAL, ACCESS_CODE)
+if __name__ == '__main__':
+    print('Starting bambulabs_api example')
+    print('Connecting to Bambulabs 3D printer')
+    print(f'IP: {IP}')
+    print(f'Serial: {SERIAL}')
+    print(f'Access Code: {ACCESS_CODE}')
 
-api.connect()
+    # Create a new instance of the API
+    api = bl.Client(IP, SERIAL, ACCESS_CODE)
 
-api.loop_forever()
+    # Connect to the Bambulabs 3D printer
+    api.connect()
+
+    # Start the API loop
+    api.loop_forever()
